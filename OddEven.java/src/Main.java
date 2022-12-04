73394f4b8f8f623d04bd60b3cfe790857a03d656
Learn <manujacsit18@oic.edu.np>
2022-12-04 14:58:57 +0545

    Initial COmmit

---
+++ OddEven.java/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        int num=0,j=1;
+      do {
+            num++;
+            if (num % 2 == 0) {
+                System.out.println((j++)+")  even "+num);
+            }
+        }  while(num<20);
+        num=0;
+        j=1;
+        do {
+            num++;
+            if(num%2!=0){
+                System.out.println((j++)+")  odd number "+num);
+            }
+        }while(num<20);
+    }
+
+}public class Main {
+    public static void main(String[] args) {
+        int num=0,j=1;
+      do {
+            num++;
+            if (num % 2 == 0) {
+                System.out.println((j++)+")  even "+num);
+            }
+        }  while(num<20);
+        num=0;
+        j=1;
+        do {
+            num++;
+            if(num%2!=0){
+                System.out.println((j++)+")  odd number "+num);
+            }
+        }while(num<20);
+    }
+
+}
